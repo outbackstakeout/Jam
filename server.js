b9bed77613e9ed96f7e58e54001e24b38f49d3a4
TheForeverOptimist <sean@elevate.app>
2023-04-26 16:05:51 -0400

    some funky socket stuff

--- server.js
+++ server.js
@@ -30,3 +30,24 @@ app.get("/*", function (req, res) {
 app.listen(port, function () {
     console.log(`Express app running on port ${port}`);
 });
+
+const io = require('socket.io')(3000, {
+    cors: {
+        origin: ["http://localhost:8080"],
+    },
+})
+
+io.on('connection', socket => {
+    console.log(socket.id)
+    socket.on("send-message", (message, room) => {
+        if (room === '') {
+            socket.broadcast.emit('receive', message)
+        }else {
+            socket.to(room).emit('receive', message)
+        }
+    })
+    socket.on('join-room', (room, cb) => {
+        socket.join(room)
+        cb(`Joined ${room}`)
+    })
+})
